b8ca463e13df396de533d89997b8a0227b232876
Vicente Rojas <virojas15@alumnos.utalca.cl>
2022-06-23 02:04:13 -0400

    Added

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.post('/sendNotification', (req, res) => {
 
     // make the selection of the existing classrooms
     // this prevent the url send notif.
-    const { room } = req.body;
-    if (room) {
+    const { cdf, ctf, id } = req.body;
+    if (id) {
         try {
-            db.promise().query(`INSERT INTO notification(fecha, sala) VALUES(now(), '${room}');`)
+            db.promise().query(`INSERT INTO notification(notification_date, notification_time, ref_room) VALUES('${cdf}', '${ctf}', ${id});`)
             res.status(201).send({
                 confirm: true
             })
@@ -36,7 +36,29 @@ app.get('/getNotifications', (req, res) => {
 
     // Get all notifications on the database
     // grouping by the room and add the count of the notifications.
-    res.send('getNotifications Works!');
+    db.query('SELECT b.building_name as building, r.room_name as room, n.notification_reason as reason, COUNT(*) as count FROM notification n, building b, room r WHERE n.ref_room = r.room_id AND r.ref_building = b.building_id GROUP BY r.room_name, n.notification_reason;', (err, rows, fields) => {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+})
+
+app.post('/getIdRoom', (req, res) => {
+    // THIS GET IS ONLY FOR ADMIN VIEW USE
+
+    // Get all notifications on the database
+    // grouping by the room and add the count of the notifications.
+    const { room } = req.body;
+    let query = "SELECT room_id FROM room WHERE room.room_name = '" + room + "';";
+    db.query(query, (err, rows, fields) => {
+        if (!err) {
+            res.send(rows[0]);
+        } else {
+            console.log(err);
+        }
+    });
 })
 
 app.get('/getHistory', (req, res) => {
@@ -45,6 +67,46 @@ app.get('/getHistory', (req, res) => {
     // Get the history of actions maked on the admin view
     // like the repair of classrooms and the recharge of dispensers.
     res.send('getHistory Works!');
+})
+
+app.get('/getClassrooms', (req, res) => {
+    // THIS GET IS ONLY FOR ADMIN VIEW USE
+
+    // Get all classroom to add to the classrooms view
+    db.query('SELECT * FROM room;', (err, rows, fields) => {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+})
+
+app.get('/getBuildings', (req, res) => {
+    // THIS GET IS ONLY FOR ADMIN VIEW USE
+
+    // Get all classroom to add to the classrooms view
+    db.query('SELECT * FROM building;', (err, rows, fields) => {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+})
+
+app.post('/postClassroom', (req, res) => {
+    // THIS POST IS ONLY FOR ADMIN VIEW USE
+
+    // Post a new classroom to being added to the database
+    res.send('postClassroom Works!');
+})
+
+app.post('/postBuilding', (req, res) => {
+    // THIS POST IS ONLY FOR ADMIN VIEW USE
+
+    // Post a new classroom to being added to the database
+    res.send('postClassroom Works!');
 })
 
 app.post('/postRR', (req, res) => {
@@ -56,21 +118,6 @@ app.post('/postRR', (req, res) => {
     // history table.
     res.send('postRR Works!');
 })
-
-app.get('/getClassrooms', (req, res) => {
-    // THIS GET IS ONLY FOR ADMIN VIEW USE
-
-    // Get all classroom to add to the classrooms view
-    res.send('getClassrooms Works!');
-})
-
-app.post('/postClassroom', (req, res) => {
-    // THIS POST IS ONLY FOR ADMIN VIEW USE
-
-    // Post a new classroom to being added to the database
-    res.send('postClassroom Works!');
-})
-
 
 
 app.listen(3000, () => {
