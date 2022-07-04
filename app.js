f9fe777428c97303cee0ee4a025808ecf7a12d01
Vicente Rojas <virojas15@alumnos.utalca.cl>
2022-07-04 05:18:32 -0400

    Release V1.0.0-Alpha

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ app.get('/getNotifications', async (req, res) => {
 })
 
 // Status: COMPLETED
-// Description: Get all rooms
+// Description: Get all rooms of a build
 app.get('/getRooms', async (req, res) => {
     let id = req.query.building_id;
     try {
@@ -111,6 +111,24 @@ app.get('/getRooms', async (req, res) => {
         db.query(query, (err, rows, fields) => {
             if (!err) {
                 res.send(rows)
+            } else {
+                console.log(err)
+            }
+        })
+    } catch (err) {
+     
+    }
+})
+
+// Status: COMPLETED
+// Description: Get a specific room by name
+app.get('/getRoomByName', async (req, res) => {
+    let name = req.query.room_name
+    try {
+        const query = "SELECT * FROM room WHERE room_name = '" + name + "';";
+        db.query(query, (err, rows, fields) => {
+            if (!err) {
+                res.send(rows[0])
             } else {
                 console.log(err)
             }
@@ -155,24 +173,6 @@ app.get('/getBuildsName', async (req, res) => {
 })
 
 // Status: COMPLETED
-// Description: Get all rooms
-app.get('/getBuilds', async (req, res) => {
-    try {
-        const query = 'SELECT building_name as name FROM building';
-        db.query(query, (err, rows, fields) => {
-            if (!err) {
-                res.send(rows)
-            } else {
-                console.log(err)
-            }
-        })
-    } catch (err) {
-        
-    }
-})
-
-
-// Status: COMPLETED
 // Description: Get complete history
 app.get('/getHistory', async (req, res) => {
     try {
@@ -207,10 +207,21 @@ app.get('/getUser', async (req, res) => {
     }
 })
 
+app.post('/getIdRoom', async (req, res) => {
+    const { room } = req.body;
+    let query = "SELECT room_id FROM room WHERE room.room_name = '" + room + "';";
+    db.query(query, (err, rows, fields) => {
+        if (!err) {
+            res.send(rows[0]);
+        } else {
+            console.log(err);
+        }
+    });
+})
 // --- END GET LIST ---
 
 // --- START POST LIST ---
-app.post('/sendNotification', (req, res) => {
+app.post('/sendNotification', async (req, res) => {
     const { room, action, comment } = req.body;
     if (room && action) {
         try {
@@ -234,37 +245,185 @@ app.post('/sendNotification', (req, res) => {
     }
 })
 
-app.post('/getIdRoom', (req, res) => {
-    const { room } = req.body;
-    let query = "SELECT room_id FROM room WHERE room.room_name = '" + room + "';";
-    db.query(query, (err, rows, fields) => {
-        if (!err) {
-            res.send(rows[0]);
-        } else {
-            console.log(err);
-        }
-    });
-})
-
-app.post('/postRR', (req, res) => {
+app.post('/postRR', async (req, res) => {
+    let { build, room, date, time, reason, count, room_id } = req.body;
+    console.log(req.body)
+    if (build && room && date && time && reason && count && room_id) {
+        try {
+            const query = "DELETE FROM notification WHERE notification_reason = '" + reason + "' AND ref_room = " + room_id +";";
+            db.promise().query(query);
     
-    res.send('postRR Works!');
+            const query2 = "INSERT INTO history (history_building, history_room, history_date, history_time, history_action, history_count) VALUES ('" + build + "', '" + room + "', '" + date + "', '" + time + "', '" + reason + "', '" + count + "')";
+            db.promise().query(query2);
+            res.status(200).send({
+                confirm: true
+            })
+        } catch (error) {
+            res.status(400).send({
+                confirm: false
+            })
+        }
+    } else {
+        res.status(400).send({
+            confirm: false
+        })
+    }
+    
+    // Hay que deletear y luego agregar con la misma info
 })
 
-app.post('/postClassroom', (req, res) => {
-    // THIS POST IS ONLY FOR ADMIN VIEW USE
-
-    // Post a new classroom to being added to the database
-    res.send('postClassroom Works!');
+// Status: COMPLETED
+// Description: Post a new Room 
+app.post('/postRoom', async (req, res) => {
+    const { name, capacity, refBuild } = req.body;
+    if (name && capacity && refBuild) {
+        try {
+            const query = "INSERT INTO room (room_name, room_capacity, ref_building) VALUES ('"+ name +"', "+ capacity +", "+ refBuild +");";
+            db.promise().query(query)
+            res.status(201).send({
+                confirm: true
+            })
+        } catch (err) {
+            res.status(400).send({
+                confirm: false
+            })
+            console.log("DATABASE LOGGER: " + err);
+        }
+    }
+    else{
+        res.status(400).send({
+            confirm: false,
+            comment: 'You need to select a building name and nickname'
+        })
+    }
 })
 
-app.post('/postBuilding', (req, res) => {
-    // THIS POST IS ONLY FOR ADMIN VIEW USE
+// Status: COMPLETED
+// Description: Post a new Build 
+app.post('/postBuilding', async (req, res) => {
+    const { name, nick } = req.body;
+    if (name && nick) {
+        try {
+            const query = "INSERT INTO building (building_name, building_nick) VALUES ('"+ name +"', '"+ nick +"');";
+            db.promise().query(query)
+            res.status(201).send({
+                confirm: true
+            })
+        } catch (err) {
+            res.status(400).send({
+                confirm: false
+            })
+            console.log("DATABASE LOGGER: " + err);
+        }
+    }
+    else{
+        res.status(400).send({
+            confirm: false,
+            comment: 'You need to select a building name and nickname'
+        })
+    }
+})
+// --- END POST LIST ---
 
-    // Post a new classroom to being added to the database
-    res.send('postClassroom Works!');
+// --- START PATCH LIST ---
+app.patch('/editBuilding', async (req, res) => {
+    const { id, name, nick } = req.body;
+    if (id && name && nick) {
+        try {
+            const query = "UPDATE building SET building_name = '" + name + "', building_nick = '" + nick + "' WHERE building_id = '" + id + "'";
+            db.promise().query(query)
+            res.status(200).send({
+                confirm: true
+            })
+        } catch (err) {
+            res.status(400).send({
+                confirm: false
+            })
+            console.log("DATABASE LOGGER: " + err);
+        }
+    }
+    else{
+        res.status(400).send({
+            confirm: false,
+            comment: 'You need to select a building name and nickname'
+        })
+    }
 })
 
+app.patch('/editRoom', async (req, res) => {
+    const { id, name, capacity, refBuild } = req.body;
+    if (id && name && capacity && refBuild) {
+        try {
+            const query = "UPDATE room SET room_name = '" + name + "', room_capacity = " + capacity + ", ref_building = " + refBuild + " WHERE room_id = '" + id + "'";
+            db.promise().query(query)
+            res.status(200).send({
+                confirm: true
+            })
+        } catch (err) {
+            res.status(400).send({
+                confirm: false
+            })
+            console.log("DATABASE LOGGER: " + err);
+        }
+    }
+    else{
+        res.status(400).send({
+            confirm: false,
+            comment: 'You need to select a building name and nickname'
+        })
+    }
+})
+// --- END PATCH LIST ---
+
+// --- START DELETE LIST ---
+app.delete('/deleteBuilding', async (req, res) => {
+    const id = req.query.id;
+    if (id) {
+        try {
+            const query = "DELETE FROM building WHERE building_id = " + id + ';';
+            db.promise().query(query)
+            res.status(200).send({
+                confirm: true
+            })
+        } catch (err) {
+            res.status(400).send({
+                confirm: false
+            })
+            console.log("DATABASE LOGGER: " + err);
+        }
+    }
+    else{
+        res.status(400).send({
+            confirm: false,
+            comment: 'You need to select a building name and nickname'
+        })
+    }
+})
+
+app.delete('/deleteRoom', async (req, res) => {
+    const id = req.query.id;
+    if (id) {
+        try {
+            const query = "DELETE FROM room WHERE room_id = " + id + ';';
+            db.promise().query(query)
+            res.status(200).send({
+                confirm: true
+            })
+        } catch (err) {
+            res.status(400).send({
+                confirm: false
+            })
+            console.log("DATABASE LOGGER: " + err);
+        }
+    }
+    else{
+        res.status(400).send({
+            confirm: false,
+            comment: 'You need to select a building name and nickname'
+        })
+    }
+})
+// --- END DELETE LIST ---
 
 app.listen(3000, () => {
     console.log('Server is running on Port 3000');
